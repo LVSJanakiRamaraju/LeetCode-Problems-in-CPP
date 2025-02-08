399910fa36a2207587bdb5193b997b6c7f09c496
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2025-02-08 13:18:49 +0530

    Time: 0 ms (100%), Space: 82.2 MB (56.5%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.cpp
+++ 2105-watering-plants-ii/2105-watering-plants-ii.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     int minimumRefill(vector<int>& plants, int capacityA, int capacityB) {
-        int A = capacityA, B = capacityB, n = plants.size();
-        int result=0;
-        for(int i=0; i<(n+1)/2; i++){
-            if(i == n-i-1){
-                if(A < plants[i] && B < plants[i])  result++;
-                break;
-            }
-            if(plants[i] > A){
-                result++;
-                A = capacityA;
-            }
-            else    A -= plants[i];
-            if(plants[n-i-1] > B){
-                result++;
-                B = capacityB;
-            }
-            else    B -= plants[n-i-1];
+        int i = 0, j = plants.size() - 1, A = capacityA, B = capacityB,
+            result = 0;
+        while (i < j) {
+            result += (A < plants[i]) + (B < plants[j]);
+            A = A < plants[i] ? capacityA : A;
+            B = B < plants[j] ? capacityB : B;
+            A -= plants[i++];
+            B -= plants[j--];
         }
-
-        return result;
+        return result + (i == j && max(A, B) < plants[i]);
     }
 };
