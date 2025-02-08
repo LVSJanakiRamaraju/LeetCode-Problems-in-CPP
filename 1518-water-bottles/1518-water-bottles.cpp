d9edfe2c95cdb61e462ed5e6bf55d1a63b18d401
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2025-02-08 12:34:55 +0530

    Time: 0 ms (100%), Space: 8 MB (8.47%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int result = numBottles + (numBottles-1)/(numExchange-1);
+        
+        return result;
+    }
+};
