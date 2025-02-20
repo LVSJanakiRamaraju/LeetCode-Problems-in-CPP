91bd113898d4835ff1ffdd2cab808e35b3a942c3
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2025-02-20 05:49:57 +0530

    Update 1980-find-unique-binary-string.cpp

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
         string result;
-    for(int i=0; i<nums.size(); i++)        result.push_back(nums[i][i] == '0' ? '1' : '0');
+        for(int i=0; i<nums.size(); i++){
+            char ch = nums[i][i] == '0' ? '1' : '0';
+            result.push_back(ch);
+        }
         return result;
         
     }
