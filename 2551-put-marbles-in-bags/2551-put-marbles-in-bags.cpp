9ef2a933da9d9993b2029864d72091d213ec5cef
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2025-03-31 09:03:12 +0530

    Time: 42 ms (30.8%), Space: 64.1 MB (91.14%) - LeetHub

---
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n = weights.size();
+        vector<int> pair(n-1, 0);
+        long long ans =0;
+        for(int i=0; i<n-1; i++){
+            pair[i] = weights[i] + weights[i+1];
+        }
+
+        sort(pair.begin(), pair.end());
+        for(int i=0; i<k-1; i++){
+            ans += pair[n-2-i] - pair[i];
+        }
+        return ans;
+    }
+};
