b719d95efa757acbb2a25f516c125a162a459eb5
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2025-08-22 14:20:42 +0530

    Time: 7 ms (63.47%), Space: 15.4 MB (25.94%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int n = s.size();
+        stack<int> st;
+        long num = 0;
+        char op = '+';
+
+        for (int i = 0; i < n; i++) {
+            char c = s[i];
+
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+
+            if ((!isdigit(c) && !isspace(c)) || i == n - 1) {
+                if (op == '+') {
+                    st.push(num);
+                } else if (op == '-') {
+                    st.push(-num);
+                } else if (op == '*') {
+                    int top = st.top(); st.pop();
+                    st.push(top * num);
+                } else if (op == '/') {
+                    int top = st.top(); st.pop();
+                    st.push(top / num);
+                }
+                op = c;
+                num = 0;
+            }
+        }
+
+        int result = 0;
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+        return result;
+    }
+};
