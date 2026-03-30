db1a2c7868e74d1c6d66fb1dbfc6fea4eaa1016d
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2026-03-30 07:22:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
 
-        vector<int> count(64, 0);
+        vector<int> count(32, 0);
 
         for (int i = 0; i < s1.length(); i++) {
-            int offset = (i & 1) << 5;
+            int offset = (i & 1) << 4;
             count[s1[i] - 'a' + offset]++;
             count[s2[i] - 'a' + offset]--;
         }
 
-        for (int i = 0; i < 64; i++) {
+        for (int i = 0; i < 32; i++) {
             if (count[i] != 0) {
                 return false;
             }
